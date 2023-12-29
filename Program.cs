7c34f6a3d16e3a30cd8937312c8277ea30cfcda3
Brandon Rivera <brandoriv.dev@gmail.com>
2023-12-29 17:13:42 -0500

    Second Commit
    This is the second commit. For testing purposes I'm adding text that yo u don't need to read :)

--- Program.cs
+++ Program.cs
@@ -2,10 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PostAutomater;
 
+//Added
+using System.Net.Http;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Add HttpClient
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
